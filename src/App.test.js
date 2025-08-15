1193238a5462d98cee7d280acd60e8c8d909c09d
MachineLearni <prasannashalini5@gmail.com>
2025-08-15 15:12:37 -0700

    Added Changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import chat from "./reducers";
+let store = createStore(chat);
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    , div);
 });
